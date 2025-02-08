b5a41402c8f22fa862ed18b0e288eafb77095b62
Wissarut Kanasub <wissarut.dev@gmail.com>
2025-02-08 16:29:29 +0700

    Database setup in app.module without any migration configuration

--- backend-nestjs/src/app.module.ts
+++ backend-nestjs/src/app.module.ts
@@ -1,11 +1,28 @@
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { Module } from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {ConfigModule} from '@nestjs/config';
+import {Module} from '@nestjs/common';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import * as process from "node:process";
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true,})],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({isGlobal: true,}),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: String(process.env.DB_HOST),
+            port: Number(process.env.DB_PORT),
+            username: String(process.env.DB_USERNAME),
+            password: String(process.env.DB_PASS),
+            database: String(process.env.DB_NAME),
+            ssl: Boolean(process.env.DB_SSL),
+            autoLoadEntities: true,
+            synchronize: true,
+        }),
+
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
