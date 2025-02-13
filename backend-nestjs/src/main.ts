5dd2d2d3719c88a3e184ac74098da8f3aeeb0050
Peanutt47 <pnchalee@gmail.com>
2025-02-13 14:57:22 +0700

    Enable CORS

--- backend-nestjs/src/main.ts
+++ backend-nestjs/src/main.ts
@@ -3,6 +3,10 @@ import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.FRONTEND_URL, // Adjust accordingly
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
